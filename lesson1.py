f53ce7821ca28683cf53d173a8f782107b8bf7a2
Reda452 <redasakhi0@gmail.com>
2025-01-21 19:17:29 +0100

    new1

---
+++ lesson1.py
@@ -0,0 +1,13 @@
+#Take user input 
+a = input("Enter a word: ")
+a = a.upper()
+print(a)
+#program to check break keyword
+for i in a: #iterating for loop 
+    if (i == 'A'): condition1
+    #display result
+    print ("A found")
+    break #break statement
+else:
+    #display result
+    print ("A not found")
